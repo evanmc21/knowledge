97bfeb65be63f40aa90d9ab6d55983767e8da5ce
evanmc21 <emcandler21@gmail.com>
2018-10-26 11:43:13 -0400

    added pug template

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const path = require('path');
+// init app
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hay gurll');
-});
+// load view engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug');
 
+// Home Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+// start server
 app.listen(3000, () => {
   console.log('Server started on port 3000...')
 });
