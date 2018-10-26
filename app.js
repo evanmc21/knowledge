497697da5672802af0c82eb1b7517e212b16306e
evanmc21 <emcandler21@gmail.com>
2018-10-26 13:15:09 -0400

    connected to the database and fetching articles

--- app.js
+++ app.js
@@ -6,38 +6,37 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/nodekb')
 let db = mongoose.connection;
 
+// check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// check for DB errors
+db.on('error', () => {
+  console.log(err);
+});
+
 // init app
 const app = express();
 
+// import models
+let Article = require('./models/article')
 // load view engine
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug');
 
 // Home Route
 app.get('/', (req, res) => {
-  let articles = [
-    {
-      id:1,
-      title:'Article One',
-      author: 'Evan Candler',
-      body: 'this is article one'
-    },
-    {
-      id:2,
-      title:'Article One',
-      author: 'Evan Michael',
-      body: 'this is article two'
-    },
-    {
-      id:3,
-      title:'Article One',
-      author: 'Evan Candler',
-      body: 'this is article three'
+  Article.find({}, (err, articles) => {
+    // check for errors
+    if(err){
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
     }
-  ];
-  res.render('index', {
-    title: 'Articles',
-    articles: articles
   });
 });
 
