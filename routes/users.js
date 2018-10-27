6c6f80413be7fe45d9df9bf3a5edb9807e9fd536
evanmc21 <emcandler21@gmail.com>
2018-10-27 01:21:22 -0400

    updated validation errors for signup and login. updated navbar

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
 
 // import User model
 let User = require('../models/user');
@@ -8,5 +10,70 @@ let User = require('../models/user');
 router.get('/signup', (req, res) => {
   res.render('signup');
 })
+
+// signup route
+router.post('/signup', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const password2 = req.body.password2;
+
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Sorry, Charlie. It looks like that is not a valid email.').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req.checkBody('password2', "It looks like the passwords don't match").notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('signup', {
+      errors: errors
+    });
+  } else {
+    let newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password
+    });
+
+// generate salt
+    bcrypt.genSalt(10, (err, salt) => {
+      // password is stored in a hash.
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err){
+          console.log(err);
+        }
+        newUser.password = hash;
+        newUser.save((err) => {
+          if(err){
+            console.log(err);
+            return;
+          } else {
+            req.flash('success', "You've successfully signed up! Please login.")
+            res.redirect('/users/login');
+          }
+        });
+      });
+    });
+  }
+  router.get('/login', (req, res) => {
+    res.render('login');
+  });
+});
+
+  router.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+
+    let errors = req.validationErrors();
+
+  })
 
 module.exports = router;
