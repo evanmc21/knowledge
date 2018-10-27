76030b40a6eb61357d69561ee23072329b431f7f
evanmc21 <emcandler21@gmail.com>
2018-10-26 21:59:23 -0400

    can now edit article

--- app.js
+++ app.js
@@ -79,6 +79,35 @@ app.get('/article/:id', (req, res) => {
   });
 });
 
+// edit single article
+app.get('/article/edit/:id', (req, res) => {
+  Article.findById(req.params.id, (err, article) => {
+    res.render('edit_article', {
+      title: 'Edit Article',
+      article: article
+    });
+  });
+});
+
+// update single article
+app.post('/articles/edit/:id', (req, res) => {
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.updateOne(query, article, (err) => {
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
 // start server
 app.listen(3000, () => {
   console.log('Server started on port 3000...')
